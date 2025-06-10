c5f3cd8a1926331773ebcf9ae79390716c763133
Laptop Ju <juliette.meyniel@epita.fr>
2025-06-10 16:30:52 +0200

    [feat] : an extract Inds to a SNPmatrix<SNPdosageDisk> working seems to have something fishy going on with the snp.stats afterward will be the fix in the next commit

--- inst/include/gaston2/extractIndsfromDosagematrixDisk.h
+++ inst/include/gaston2/extractIndsfromDosagematrixDisk.h
@@ -9,70 +9,62 @@
 #ifndef _EXTRACTINDDOSMATRIX_DISK_
 #define _EXTRACTINDDOSMATRIX_DISK_
 
-template <typename SNPvectorClass, typename intVec>
-void extractIndsfromDosagematrixDisk(const SNPmatrix<SNPvectorClass> &other, const intVec &keep, std::string path_str,
-                                  SNPmatrix<SNPvectorClass> &newMat) {
-  throw std::runtime_error("I don't have the Inds extracting for a dosage matrix on file ready yet !");
+// To be sure that I'm only extracting from a SNPmatrix with dosages,
+// I removed the template of the SNPmatrix (previously SNPmatrix<SNPvectorClass>)
+// 
+
+template <typename intVec>
+void extractIndsfromDosagematrixDisk(const SNPmatrix<SNPdosage> &other, const intVec &keep, std::string path_str,
+                                  SNPmatrix<SNPdosage> &newMat) {
+  std::error_code error;
+
+  /* FIRST : check if file exists, if it does, abort to not overwrite */
+  const char * path_c = path_str.c_str();
+  FILE *check = fopen(path_c, "rb");
+  if (check) {
+      fclose(check);
+      throw std::runtime_error("The new matrix will overwrite an existing file, Aborting !");
+  }
+
+  /* THEN if it doesn't exist, go prepare it with the right size for mio */
+  FILE *f = fopen(path_c, "wb");
+  if (!f)
+  {
+    throw std::runtime_error("Failed to open file for writing");
+  }
+
+  const std::vector<std::shared_ptr<SNPdosage>> otherSNPs = other.getSNPs();
+  
+  int to_add = (keep.size() * sizeof(float)) * otherSNPs.size();
+  if (fseek(f, to_add - 1, SEEK_SET) != 0)
+  {
+    fclose(f);
+    throw std::runtime_error("Error when resizing file");
+  }
+  fputc(0, f); // this is what will size it up
+  fclose(f);
+
+  /* NOW : mapping the created file with mio, */
+  std::shared_ptr<mio::mmap_sink> file_ptr = std::make_shared<mio::mmap_sink>(mio::make_mmap_sink(path_str, 0, mio::map_entire_file, error));
+  if (error)
+  {
+    std::string errMsg = "Error code " + std::to_string(error.value()) + ", Failed to map the file : " + error.message();
+    throw std::runtime_error(errMsg);
+  }
+
+  size_t SNPindex = 0;
+
+  for (const auto &snp : otherSNPs){
+    newMat.push_back(std::make_shared<SNPdosageDisk<mio::access_mode::write>>(snp, file_ptr, SNPindex++, keep));
+  }
+  //extract stats now and set stats_set_ to true
+  DataStruct original_dt = other.getIndStats();
+  newMat.setIndStats(DataStruct(original_dt, keep));
 }
 
-// template <typename SNPvectorClass, typename intVec>
-// void extractIndsfromSNPmatrixDisk(const SNPmatrix<SNPvectorClass> &other, const intVec &keep, std::string path_str,
-//                                   SNPmatrix<SNPvectorClass> & newMat) {
-
-//   std::error_code error;
-
-//   /* FIRST : check if file exists, if it does, abort to not overwrite */
-//   const char * path_c = path_str.c_str();
-//   FILE *check = fopen(path_c, "rb");
-//   if (check) {
-//       fclose(check);
-//       throw std::runtime_error("The new matrix will overwrite an existing file, Aborting !");
-//   }
-
-//   /* THEN if it doesn't exist, go prepare it for mio with .bed's magic bytes */
-//   FILE *f = fopen(path_c, "wb");
-//   if (!f)
-//   {
-//     throw std::runtime_error("Failed to open file for writing");
-//   }
-//   // Adding magic numbers to Identify a bed file in SNP major mode
-//   fputc(108, f);
-//   fputc(27, f);
-//   fputc(1, f);
-
-//   const std::vector<std::shared_ptr<SNPvector>> otherSNPs = other.getSNPs();
-
-//   // (+ 3 for the 3 magic bytes)
-//   int to_add = (keep.size() / 4 + ((keep.size() % 4 == 0u) ? 0 : 1)) * otherSNPs.size() + 3 ;
-//   if (fseek(f, to_add - 1, SEEK_SET) != 0)
-//   {
-//     fclose(f);
-//     throw std::runtime_error("Error when resizing file");
-//   }
-//   fputc(0, f); // this is what will size it up
-//   fclose(f);
-
-//   /* NOW : mapping the created file with mio, */
-//   std::shared_ptr<mio::mmap_sink> file_ptr = std::make_shared<mio::mmap_sink>(mio::make_mmap_sink(path_str, 0, mio::map_entire_file, error));
-//   if (error)
-//   {
-//     std::string errMsg = "Error code " + std::to_string(error.value()) + ", Failed to map the file : " + error.message();
-//     throw std::runtime_error(errMsg);
-//   }
-
-//   size_t SNPindex = 0;
-
-//   for (const auto &snp : otherSNPs){
-//     newMat.push_back(std::make_shared<SNPvectorDisk<mio::access_mode::write>>(snp, file_ptr, SNPindex++, keep));
-//   }
-//   //extract stats now and set stats_set_ to true
-//   DataStruct original_dt = other.getIndStats();
-//   newMat.setIndStats(DataStruct(original_dt, keep));
-// }
-
-template <typename SNPvectorClass, typename intVec>
-SNPmatrix<SNPvectorClass> extractIndsfromDOSmatrixDisk(const SNPmatrix<SNPvectorClass> &other, const intVec &keep, std::string path_str) {
-  SNPmatrix<SNPvectorClass> M;
+template <typename intVec>
+SNPmatrix<SNPdosage> extractIndsfromDosagematrixDisk(const SNPmatrix<SNPdosage> &other, const intVec &keep, std::string path_str) {
+  SNPmatrix<SNPdosage> M;
   extractIndsfromDosagematrixDisk(other, keep, path_str, M);
   return M;
 }
